18616b661ec511ab476a85e5cad8b8ef4d9cb8e2
Radical <benjaminjohnsen.bgj@gmail.com>
2022-10-17 13:17:37 +0200

    Fix indentation

--- src/main.rs
+++ src/main.rs
@@ -56,9 +56,9 @@ impl epi::App for PasswordGenerator {
 }
 
 fn main() {
-let app = PasswordGenerator::default();
-let native_options = eframe::NativeOptions::default();
-eframe::run_native(Box::new(app), native_options);
+    let app = PasswordGenerator::default();
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(Box::new(app), native_options);
 }
 
 fn pwdgen(lowercase: bool, uppercase: bool, numbers: bool, symbols: bool, length: i32) -> String {
